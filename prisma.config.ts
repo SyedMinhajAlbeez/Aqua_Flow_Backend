b34d6d079c347f1f9f7de83ccc3ff0fb2afbe5b4
SyedMinhajAlbeez <147368067+SyedMinhajAlbeez@users.noreply.github.com>
2025-12-09 10:25:27 +0500

    update_prisma_config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,9 @@
-// prisma.config.ts
 import "dotenv/config";
+import { defineConfig, env } from "@prisma/config";
 
-export default {
-  schema: "prisma/schema.prisma",
-};
+export default defineConfig({
+  schema: "./prisma/schema.prisma",
+  datasource: {
+    url: env("DIRECT_URL"), // Yeh CLI ke liye direct use karega
+  },
+});
